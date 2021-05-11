c53d2878e7eea5de310bcfa96f3b472347cedda3
VazkenA98 <vazken99@gmail.com>
2021-05-11 22:49:55 +0400

    button login

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,22 +7,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class main {
     public static ChromeDriver chromeDriver = null;
-    public static void main(String[] args) {
 
+    public static void main(String[]args){
         System.setProperty("webdriver.chrome.driver","/Users/vazkenabdulian/Downloads/chromedriver");
-        chromeDriver =  new ChromeDriver();
-
+        chromeDriver = new ChromeDriver();
+        //chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         chromeDriver.get("http://picsartstage2.com/");
-
         WebElement loginBut =  chromeDriver.findElement(By.cssSelector("[data-test='headerAuth-signInBtn pa-uiLib-headerAuth-authBtn']"));
         loginBut.click();
-        new WebDriverWait(chromeDriver, 10).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.name("username")));
-        login("vazken","Abdulian");
+        new WebDriverWait(chromeDriver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        login("Vazken", "123");
 
     }
+
     public static void login(String name, String password){
         chromeDriver.findElement(By.name("username")).sendKeys(name);
         chromeDriver.findElement(By.name("password")).sendKeys(password);
-
     }
 }
