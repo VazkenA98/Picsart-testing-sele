f4a30638d8db35c558c18e899940c59904c17df8
VazkenA98 <vazken99@gmail.com>
2021-05-12 18:25:16 +0400

    updated

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,22 +6,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class main {
+
     public static ChromeDriver chromeDriver = null;
 
     public static void main(String[]args){
-        System.setProperty("webdriver.chrome.driver","/Users/vazkenabdulian/Downloads/chromedriver");
+        onStart();
+        clickLoginButton();
+        new WebDriverWait(chromeDriver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        WebElement clickLogIn = chromeDriver.findElement(By.cssSelector("[class*='pa-uiLib-authentication-btn primary']"));
+        login("Vazken", "123",clickLogIn);
+    }
+    public static void onStart(){
+        System.setProperty("webdriver.chrome.driver","/Users/nubar/Downloads/chromedriver");
         chromeDriver = new ChromeDriver();
-        //chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         chromeDriver.get("http://picsartstage2.com/");
+    }
+    public static void clickLoginButton(){
         WebElement loginBut =  chromeDriver.findElement(By.cssSelector("[data-test='headerAuth-signInBtn pa-uiLib-headerAuth-authBtn']"));
         loginBut.click();
-        new WebDriverWait(chromeDriver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        login("Vazken", "123");
-
     }
-
-    public static void login(String name, String password){
+    public static void clickAction(WebElement element){
+        new WebDriverWait(chromeDriver, 10).until(ExpectedConditions.visibilityOf(element));
+        element.click();
+    }
+    public static void login(String name, String password, WebElement element) {
         chromeDriver.findElement(By.name("username")).sendKeys(name);
         chromeDriver.findElement(By.name("password")).sendKeys(password);
+        if (element.isDisplayed() && element.isEnabled()) {
+            System.out.println("Clicking.....");
+            clickAction(element);
+        }
     }
 }
